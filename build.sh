4f353fe740ef8513b2d5fcc51b577163e6255b82
Benjamin Riggs <deisum@gmail.com>
2016-02-03 13:07:43 -0500

    Slightly more legit build file.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+rm -r build
+mkdir build
+
 browserify --debug interface_wrapper.js -o build/interface_wrapper.js
 browserify --debug kurento_wrapper.js -o build/kurento_wrapper.js
 browserify --debug kurento_webview.js -o build/kurento_webview.js
